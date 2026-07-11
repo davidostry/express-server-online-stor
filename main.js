b5167c1c5e5c558f83d011d89a464139e83698d0
david <sarigoo421@gmail.com.il>
2026-07-12 02:14:30 +0300

    main written successfully

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cartRouter from './routes/cartRouter.js'
 import productsRouter from './routes/productsRouter.js'
 import ordersRouter from './routes/ordersRouter.js'
+import {getAllCustomers} from './services/costomerService.js'
 
 const app = express()
 
@@ -14,15 +15,23 @@ app.use("/products", productsRouter)
 app.use("/orders", ordersRouter)
 
 app.get("/", (req, res)=>{
-    res.end("Wellcome to the online clothing store")
+    res.json({"success": true, "message": "Wellcome to the online clothing store"})
 })
 
 
 app.get("/health", (req, res)=>{
-    res.end("The server is runing properly")
+    res.json({"success": true, "message": "The server is runing properly"})
 })
 
-
+app.get("/account/balance", async (req, res)=>{
+    const { customerId } = req.query
+    if (!customerId || isNaN(+customerId)) return res.status(400).json({"success": false, "message": "missing customerId"})
+    const customers = await getAllCustomers()
+    const customer = customers.find(c=>c.customerId === +customerId)
+    if(!customer) return res.status(400).json({"success": false, "message": "customerId not found"})
+    res.json({"success": true, "data": customer.balance})
+    
+})
 
 app.listen(3000, ()=>{                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
     console.log("server runing...");
